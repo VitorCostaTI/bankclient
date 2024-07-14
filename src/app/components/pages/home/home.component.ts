0ab22ceea783b011a6f4edf43c338cb39b65c331
VitorCostaTI <vitortecnologia62@gmail.com>
2024-07-14 05:16:03 -0300

    feat: cabeçalho páginas

---
+++ src/app/components/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MainHeaderComponent } from '../../main-header/main-header.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [ MainHeaderComponent ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
