d630ab01b03baed22eaf13283031a951b994ca07
VitorCostaTI <vitortecnologia62@gmail.com>
2024-07-15 03:46:19 -0300

    Estilização botão transferencia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { IconTransferComponent } from './components/icon-transfer/icon-transfer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, IconTransferComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
